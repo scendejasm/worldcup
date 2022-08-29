a322c1493698b185850b1b34546840e3aee6f737
scendejasm <scendejasm@gmail.com>
2022-08-29 18:33:23 +0000

    initial commit

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,54 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+# clear table 
+echo $($PSQL "TRUNCATE teams, games")
+
+# read in each line from games.csv to populate teams table
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $WINNER != 'winner' ]]
+  then
+    # get team_id of WINNER 
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    # if not found
+    if [[ -z $WINNER_ID ]]
+    then
+      # insert team WINNER_ID
+      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERT_WINNER_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted winner into teams, $WINNER
+      fi
+      # get new WINNER_ID
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    fi 
+  fi
+  # get team_id of OPPONENT
+  if [[ $OPPONENT != 'opponent' ]]
+  then 
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    
+    # if not found 
+    if [[ -z $OPPONENT_ID ]]
+    then
+      # insert OPPONENT team 
+      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted $OPPONENT into teams
+      fi
+      # get new opponent_id
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    fi
+  fi
+  # populate the tames table 
+  if [[ $YEAR != 'year' ]]
+  then
+    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+    if [[ $INSERT_GAME_RESULT == 'INSERT 0 1' ]]
+    then
+        echo -e "\nAdding game between $WINNER and $OPPONENT played in $YEAR with team ids $WINNER_ID, $OPPONENT_ID"
+    fi
+  fi
+done
